cb45f62c69df602726fe72fb73740cae91f3df32
Al Wong <timberhead1121@gmail.com>
2022-10-06 09:57:04 -0600

    completed config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,9 +9,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-const WorkboxPlugin = require("workbox-webpack-plugin");
-const CSSWebpackPlugin = require("workbox-webpack-plugin");
-const babelWebpackPlugin = require("workbox-webpack-plugin");  
+ 
 
 module.exports = () => {
   return {
@@ -25,12 +23,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+    
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "jate"
+      }),
+
+      new WebpackPwaManifest({
+        name: "i-am-groot-text-editor",
+        short_name: "jate",
+        description: "long live jate",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ]
+      }),
+
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+           use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+            },
+          }
+        }
       ],
     },
   };
